6d32b5122d2db11eab30d6b1f1515d6c6ffd3b82
stupormundi3 <ziad.iziz@student.unamur.be>
2025-10-30 19:32:44 +0100

    Un peu de debugging et implémentation partiel en jax, je dois encore tester avec les kernels de Kernax

--- project/SVM/SimpleClassificationProblem.py
+++ project/SVM/SimpleClassificationProblem.py
@@ -1,11 +1,15 @@
 import numpy as np
+import jax
+import jax.numpy as jnp
+from jax import jit
 import matplotlib
 matplotlib.use('svg')
 import matplotlib.pyplot as plt
 from sklearn import svm
 from matplotlib import cm
+from legacy.Kernax.LinearKernel import LinearKernel
 
-# Je dois encore convertir en jax
+# Still need to convert in jax
 # Suppose there is a circle with center at (0, 0) and radius 1.2.
 # All the points within the circle are labeled 1.
 # All the points outside the circle are labeled 0.
@@ -13,11 +17,21 @@ nSamples = 10
 spanLen = 2
 X = np.zeros((nSamples, 2))
 y = np.zeros((nSamples, ))
-
+c = 0
+# Generation of our dataset, each point can either have 1 or 0 as his class.
 for i in range(nSamples):
+ 
+
   a, b = [np.random.uniform(-spanLen, spanLen) for _ in ['x', 'y']]
+  print(a)
+  print(f"A is above, iteration{c}"  )
+  print(b)
+  print(f"B is above, iteration {c}")
+  c = c+1
   X[i][0], X[i][1] = a, b
   y[i] = 1 if a*a + b*b < 1.2*1.2 else 0
+print(X)  
+  
 
 # Custom kernel,
 def my_kernel(A, B):
@@ -34,14 +48,14 @@ def my_kernel(A, B):
   return gram
 
 # SVM train.
-clf = svm.SVC(kernel = my_kernel)
+clf = svm.SVC(kernel = jit(my_kernel))
 clf.fit(X, y)
 coef = clf.dual_coef_[0]
 sup = clf.support_
 b = clf.intercept_
 x_min, x_max = -spanLen, spanLen
 y_min, y_max = -spanLen, spanLen
-xx, yy = np.meshgrid(np.arange(x_min, x_max, .02), np.arange(y_min, y_max, .02))
+xx, yy = jnp.meshgrid(np.arange(x_min, x_max, .02), np.arange(y_min, y_max, .02))
 Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
@@ -53,8 +67,8 @@ plt1.set_ylim([-spanLen, spanLen])
 plt1.set_xticks([-1, 0, 1])
 plt1.set_yticks([-1, 0, 1])
 plt1.pcolormesh(xx, yy, Z, cmap=cm.Paired)
-y_unique = np.unique(y)
-colors = cm.rainbow(np.linspace(0.0, 1.0, y_unique.size))
+y_unique = jnp.unique(y)
+colors = cm.rainbow(jnp.linspace(0.0, 1.0, y_unique.size))
 for this_y, color in zip(y_unique, colors):
   this_Xx = [X[i][0] for i in range(len(X)) if y[i] == this_y]
   this_Xy = [X[i][1] for i in range(len(X)) if y[i] == this_y]
@@ -62,7 +76,7 @@ for this_y, color in zip(y_unique, colors):
 
 # Process the training data into 3D by applying the kernel mapping:
 # phi(x, y) = (x, y, x*x + y*y).
-X3d = np.ndarray((X.shape[0], 3))
+X3d = jnp.ndarray((X.shape[0], 3))
 for i in range(X.shape[0]):
     a, b = X[i][0], X[i][1]
     X3d[i, 0], X3d[i, 1], X3d[i, 2] = [a, b, a*a + b*b]
@@ -92,17 +106,21 @@ def onBoundary(x, y, z, X3d, coef, sup, b):
     return True
   return False
 
-Xr = np.arange(x_min, x_max, .02)
-Yr = np.arange(y_min, y_max, .02)
-Z = np.zeros(Z.shape)
+Xr = jnp.arange(x_min, x_max, .02)
+Yr = jnp.arange(y_min, y_max, .02)
+Z = jnp.zeros(Z.shape)
 for i in range(Xr.shape[0]):
   x = Xr[i]
   for j in range(Yr.shape[0]):
     y = Yr[j]
-    for z in np.arange(0, 2, .02):
-      if onBoundary(x, y, z, X3d, coef, sup, b):
+    for z in jnp.arange(0, 2, .02):
+      if jit(onBoundary(x, y, z, X3d, coef, sup, b)):
         Z[i, j] = z
         break
 plt2.plot_surface(xx, yy, Z, cmap='summer', alpha=0.2)
 
 plt.savefig("kerneltrick.svg", format = "svg")
+# Important to note that this example is only an ILLUSTRATION of what the kernel trick looks like,
+# The kernel trick principle doesn't need to transform explicitly transform our dataset into a higher dimension
+# As kernels already do in implicitly by considering each dot product of each point converted into our
+# Higher dimensional space, according to the kernel used.
